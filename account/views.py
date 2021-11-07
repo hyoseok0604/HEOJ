0b2917003507ca4c86e48ae9367f4ac9fc9faab8
hyoseok <janghs0604@gmail.com>
2021-11-07 22:33:18 +0900

    제출 통계 기능 추가 및 다른  통계 지원 준비

--- account/views.py
+++ account/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.db.models import Count
 
 from account.models import Profile
+from problem.models import Submission
 from .forms import UserForm
 from django.shortcuts import render, redirect
 
@@ -38,10 +40,31 @@ def logout_request(request):
 	return redirect("/")
 
 def profile(request, username):
-    profile = Profile.objects.get(user__username=username)
+    profile = Profile.objects.get(user__username=username).select_related("user")
+
+    # 제출 결과별 통계
+    result_queryset = profile.user.submission_set.values('result').annotate(Count('result'))
+    result_labels = Submission.Result.labels
+    result_counts = [0 for _ in range(result_labels)]
+    for result_query in result_queryset:
+        result_counts[result_query['result']] = result_query['result__count']
+
+    # 제출 언어별 통계
+    language_queryset = profile.user.submission_set.values('language').annotate(Count('language'))
+    language_labels = Submission.Language.labels
+    language_counts = [0 for _ in range(language_labels)]
+    for language_query in language_queryset:
+        language_counts[language_query['language']] = language_query['language__count']
+
+    # 제출 언어별 결과 통계
+    language_result_queryset = profile.user.submission_set.values('language', 'result').annotate(Count('language', 'result')) # 언어별 결과 통계
 
     context = {
         "profile": profile,
+        "result_labels": result_labels[2:],
+        "result_counts": result_counts[2:],
+        "language_counts": 
+
     }
 
     return render(request, "account/profile.html", context)
